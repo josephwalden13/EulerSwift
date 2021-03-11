859cf3c74c31fc402a66a1c09e8049d13dbbfaad
Joseph Walden <josephwalden13@gmail.com>
2021-03-11 14:01:29 +0000

    Initial Commit

---
+++ EulerSwift/EulerSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  EulerSwiftApp.swift
+//  EulerSwift
+//
+//  Created by Joseph Walden on 11/03/2021.
+//
+
+import SwiftUI
+
+@main
+struct EulerSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
